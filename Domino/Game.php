9cb83b0e86b8e532a11167f16480af4583be26b3
Petr Kovalev <git@kovalev.pw>
2021-10-07 05:40:29 +0500

    Update Game.php
    Update

--- Domino/Game.php
+++ Domino/Game.php
@@ -1,1 +1,31 @@
+<?php
 
+namespace Domino;
+
+final class Game
+{
+    private GameRulesInterface $rules;
+
+    private PlayerList $playerList;
+
+    public function __construct(GameRulesInterface $rules, PlayerList $playerList)
+    {
+        $this->rules = $rules;
+        $this->playerList = $playerList;
+    }
+
+    public function run(): void
+    {
+
+    }
+
+    public function getPlayerList(): PlayerList
+    {
+        return $this->playerList;
+    }
+
+    public function getRules(): GameRulesInterface
+    {
+        return $this->rules;
+    }
+}
