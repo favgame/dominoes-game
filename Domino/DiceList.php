4263d4d9f5ad7d503620ccd64b0c0f4b0a49dc4e
Petr Kovalev <git@kovalev.pw>
2021-10-07 07:01:06 +0500

    Create DiceList.php

---
+++ Domino/DiceList.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Domino;
+
+final class Dice
+{
+    private DiceSide $sideA;
+
+    private DiceSide $sideB;
+
+    private ?PlayerInterface $owner = null;
+
+    public function __construct(DiceSide $sideA, DiceSide $sideB)
+    {
+        $this->sideA = $sideA;
+        $this->sideB = $sideB;
+    }
+
+    public function getSideA(): DiceSide
+    {
+        return $this->sideA;
+    }
+
+    public function getSideB(): DiceSide
+    {
+        return $this->sideB;
+    }
+
+    public function getOwner(): ?PlayerInterface
+    {
+        return $this->owner;
+    }
+
+    public function hasOwner(): bool
+    {
+        return ($this->owner !== null);
+    }
+
+    public function setOwner(PlayerInterface owner): void
+    {
+        $this->owner = $owner;
+       // TODO: exception if owned
+    }
+}
