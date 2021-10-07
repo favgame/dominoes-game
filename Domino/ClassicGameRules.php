95adf2a01f8ad7cbcc56e758cb88573202571045
Petr Kovalev <git@kovalev.pw>
2021-10-07 05:58:05 +0500

    Update ClassicGameRules.php

--- Domino/ClassicGameRules.php
+++ Domino/ClassicGameRules.php
@@ -4,17 +4,5 @@ namespace Domino;
 
 final class ClassicGameRules implements GameRulesInterface
 {
-    private const MIN_PLAYERS_COUNT = 2;
 
-    private const MAX_PLAYERS_COUNT = 4;
-
-    public function getMinPlayersCount(): int
-    {
-        return self::MIN_PLAYERS_COUNT;
-    }
-
-    public function getMaxPlayersCount(): int
-    {
-        return self::MAX_PLAYERS_COUNT;
-    }
 }
