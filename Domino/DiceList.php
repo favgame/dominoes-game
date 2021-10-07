bb6f40c996cb46648ec2865495865aceaaeb32ed
Petr Kovalev <git@kovalev.pw>
2021-10-07 08:57:05 +0500

    Update DiceList.php

--- Domino/DiceList.php
+++ Domino/DiceList.php
@@ -1,15 +1,20 @@
 <?php
 
-namespace Domino;
+namespace Domino\Dices;
+
+use ArrayObject;
 
 final class DiceList
 {
-    private array $items = [];
+    private array $items;
 
     public function addItem(Dice $dice): void
     {
         $this->items[] = $dice;
     }
 
-    public function getAll(): array
+    public function getIterator(): ArrayIterrator
+    {
+        return new ArrayItetator(,);
+    }
 }
